7249ec263892f72013192461a425d1f92f2f782a
Vu Anh Hao <hi@anhhao.me>
2022-05-31 16:24:01 +0700

    Update config, schema

--- src/schema.js
+++ src/schema.js
@@ -2,6 +2,9 @@ const schemas = [
   {
     __name: 'users',
     __type: 'mem',
+    __icon: 'person',
+    __str: 'email',
+
     email: { type: 'text', required: true, maxlength: 100 },
     password: { type: 'password', maxlength: 100 },
     apikey: { type: 'text', maxlength: 100 }
@@ -9,9 +12,18 @@ const schemas = [
   {
     __name: 'posts',
     __type: 'mem',
+    __icon: 'document-text',
+    __str: 'name',
+
     name: { type: 'text', required: true, maxlength: 100 },
     slug: { type: 'text', required: true, maxlength: 100, regex: '^[a-z0-9-_.]+$' },
     content: { type: 'text', editor: 'wysiwyg' }
+  },
+  {
+    __name: 'resources',
+    __type: 'fs',
+    __icon: 'folder-open',
+    __str: 'name'
   }
 ];
 
