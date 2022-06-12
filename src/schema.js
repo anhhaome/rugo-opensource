be01056104214aa4519071139ac9e1a0a9c45f18
Vu Anh Hao <hi@anhhao.me>
2022-06-12 18:33:46 +0700

    Add pin and relation for post

--- src/schema.js
+++ src/schema.js
@@ -3,7 +3,6 @@ const schemas = [
     __name: 'users',
     __type: 'mem',
     __icon: 'person',
-    __str: 'email',
 
     email: { type: 'text', required: true, maxlength: 100 },
     password: { type: 'password', maxlength: 100 },
@@ -13,18 +12,26 @@ const schemas = [
     __name: 'posts',
     __type: 'mem',
     __icon: 'document-text',
-    __str: 'name',
 
     name: { type: 'text', required: true, maxlength: 100 },
     slug: { type: 'text', required: true, maxlength: 100, regex: '^[a-z0-9-_.]+$' },
     image: { type: 'upload', ref: 'resources', root: '/public' },
+    category: { type: 'relation', ref: 'categories', str: 'name' },
+    pin: { type: 'boolean' },
     content: { type: 'text', editor: 'rich', upload: { ref: 'resources', root: '/public'} }
+  },
+  {
+    __name: 'categories',
+    __type: 'mem',
+    __icon: 'copy',
+
+    name: { type: 'text', required: true, maxlength: 100 },
+    slug: { type: 'text', required: true, maxlength: 100, regex: '^[a-z0-9-_.]+$' }
   },
   {
     __name: 'resources',
     __type: 'fs',
-    __icon: 'folder-open',
-    __str: 'name'
+    __icon: 'folder-open'
   }
 ];
 
