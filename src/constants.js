a551ff9b60f160034f6a655222c8267354d77c0e
Vu Anh Hao <hi@haova.me>
2023-05-02 12:47:39 +0700

    Fix import service

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,8 @@ import { fileURLToPath } from 'node:url';
 
 // cwd
 const __dirname = dirname(fileURLToPath(import.meta.url));
-let cwd = resolve(__dirname, '../');
-if (!existsSync(cwd)) cwd = resolve('./');
+let cwd = resolve(__dirname, '../node_modules');
+if (!existsSync(cwd)) cwd = resolve('./node_modules');
 
 export const CONFIG_PORT = 2023;
 export const SERVER_PORT = 8000;
@@ -18,14 +18,14 @@ export const VIEW_ENGINE = 'fx.run';
 
 export const SERVER_SERVICE = {
   name: 'server',
-  exec: ['node', './node_modules/@rugo-vn/server/src/index.js'],
+  exec: ['node', './@rugo-vn/server/src/index.js'],
   cwd,
   settings: {},
 };
 
 export const FX_SERVICE = {
   name: 'fx',
-  exec: ['node', './node_modules/@rugo-vn/fx/src/index.js'],
+  exec: ['node', './@rugo-vn/fx/src/index.js'],
   cwd,
   settings: {},
 };
